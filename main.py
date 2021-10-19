5204017bd8bc257d4a09670f8bef0e7aeb08898a
andss1 <simao.anderson4@gmail.com>
2021-10-18 23:17:19 -0300

    Add average IoU of all images of pipeline1

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ path_1 = './data/pipeline1/predict_0.png'
 path_2 = './data/pipeline1/predict_0.png'
 path_3 = './data/gold/img_0.png'
 
-print(iou_coef(read_img(path_1), read_img(path_3)))
-
+#print(iou_coef(read_img(path_1), read_img(path_3)))
+print(iou_result())
 
--- src/data_manipulation.py
+++ src/data_manipulation.py
@@ -1,17 +1,35 @@
+import os
+
 import cv2 as cv
 import numpy as np
 
 H = 128
 W = 128
 
-def iou_result(ff, d):
+
+def iou_result():
     """
     :param ff: Firefly
     :param d: Num. of weights
     :return: iou average
     """
-    iou = 0
-    return iou
+    path_pl1 = './data/pipeline1'
+    path_gold = './data/gold'
+    files_pl1 = os.listdir(path_pl1)
+    files_gold = os.listdir(path_gold)
+
+    total_img = 0
+    acc_iou = 0
+    for index, file in enumerate(files_pl1):
+        pl1 = path_pl1 + '/' + file
+        gold = path_gold + '/' + files_gold[index]
+        acc_iou = acc_iou + iou_coef(read_img(pl1), read_img(gold))
+        total_img = index
+    total_img += 1
+
+    avg_iou = acc_iou/total_img
+
+    return avg_iou
 
 
 def iou_coef(img_p, img_g):
@@ -24,7 +42,7 @@ def iou_coef(img_p, img_g):
     lin = np.size(img_p, 0)
     col = np.size(img_p, 1)
 
-    total_pixel = lin*col
+    total_pixel = lin * col
     acc = 0
 
     for n in range(lin):
@@ -32,7 +50,7 @@ def iou_coef(img_p, img_g):
             if np.array_equal(img_p[n][m], img_g[n][m]):
                 acc += 1
 
-    iou = acc/total_pixel
+    iou = acc / total_pixel
     return iou
 
 
